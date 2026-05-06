a0c109b54c9892d37cfd315e71c463a19fefedf7
Belinda47 <fanshiqi@usc.edu>
2026-05-06 15:39:08 +0800

    Add config file

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# AI generated:
+
+INSTAGRAM_DATA_PATH = "data/instagram_data.csv"
+
+TIKTOK_DATA_PATH = "data/tiktok_data.csv"
+
+YOUTUBE_DATA_PATH = "data/youtube_data.csv"
+
+YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/search"
